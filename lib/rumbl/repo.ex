67144468304a54b6a16b7c0513d8c77fe88444f6
Gabriel Kastenbaum <gabriel.kastenbaum@gmail.com>
2017-11-18 23:46:04 +0100

    Chapter 4

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,23 +1,24 @@
 defmodule Rumbl.Repo do
-  # use Ecto.Repo, otp_app: :rumbl
+  use Ecto.Repo, otp_app: :rumbl
   @moduledoc """
-  In memory repository
+  In DB repository
   """
-  def all(Rumbl.User) do
-    [
-      %Rumbl.User{id: "1", name: "José", username: "joseviala", password: "elixir"},
-      %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
-      %Rumbl.User{id: "3", name: "Chris", username: "chrismccard", password: "phx"}
-    ]
-  end
+  # def all(Rumbl.User) do
+  #   [
+  #     %Rumbl.User{id: "1", name: "José", username: "joseviala", password: "elixir"},
+  #     %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
+  #     %Rumbl.User{id: "3", name: "Chris", username: "chrismccard", password: "phx"}
+  #   ]
+  # end
+  # def all(_), do: []
 
-  def get(module, id) do
-    Enum.find all(module), fn u -> u.id==id end
-  end
+  # def get(module, id) do
+  #   Enum.find all(module), fn u -> u.id==id end
+  # end
 
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn(k, v) -> Map.get(map, k)==v end)
-    end
-  end
+  # def get_by(module, params) do
+  #   Enum.find all(module), fn map ->
+  #     Enum.all?(params, fn(k, v) -> Map.get(map, k)==v end)
+  #   end
+  # end
 end

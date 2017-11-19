b6b462fc5e31e91d7a0222eb5317a8783f181560
Gabriel Kastenbaum <gabriel.kastenbaum@gmail.com>
2017-11-19 14:45:32 +0100

    Chapter 5

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Rumbl.Mixfile do
   def application do
     [
       mod: {Rumbl, []},
-      extra_applications: [:logger]
+      extra_applications: [:logger, :comeonin]
     ]
   end
 
@@ -33,14 +33,16 @@ defmodule Rumbl.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0-rc"},
+      #{:ecto, "~> 2.2.6"},
+      {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:comeonin, "~> 2.0"}
     ]
   end
 
